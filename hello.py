6dce77ff6d5bafa034d0df7e2caedb0cf641fd26
Igor <fire.ex29@gmail.com>
2019-02-20 08:43:21 +0300

    change

--- hello.py
+++ hello.py
@@ -1,1 +1,30 @@
-print ("Hello World")
+import time
+
+hours_of_day = time.strftime("%H", time.localtime())
+hours_of_day = int (hours_of_day)
+
+hello = "Доброго " #для русской части
+
+if 6 > hours_of_day >= 0 :
+	case = "night"
+	case_ru = "ночи"
+	hello = "Доброй "
+
+elif 12 > hours_of_day >= 6 :
+	case = "morning"
+	case_ru = "утра"
+
+elif 18 > hours_of_day >= 12 :
+	case = "day"
+	case_ru = "дня"
+
+elif 24 > hours_of_day >= 18 :
+	case = "evening"
+	case_ru = "вечера"
+
+massage = "Good " + case + ", Max!"
+massage_ru = hello + case_ru + ", Макс!"
+
+print(massage)
+print(massage_ru)
+
