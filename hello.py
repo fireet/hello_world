06bced080e5b2ddd5e1c1af2dc72227f4e325ddb
Igor <fire.ex29@gmail.com>
2019-02-22 09:21:52 +0300

    сокращен словарь, устранено копирование кода, поправлена проверка условий

--- hello.py
+++ hello.py
@@ -4,32 +4,28 @@ import sys
 hours_of_day = time.strftime("%H", time.localtime())
 hours = int (hours_of_day) // 6
 
-
-# Русская часть
-
-
-#Английская часть
-
-def hello_uni(night, morning, day, evening):
-		
+# функция задает параметры аргумента и фразы приветствия
+def hello_uni(var1, var2, night, morning, day, evening):
+	verse = {var1, var2}	
 	times.update ({0:night,1:morning,2:day,3:evening})
-
-#Проверка парамtnhjd
+	if sys.argv[1] in verse:
+		print (times.get(hours))
+		exit()
+#Проверка параметров
+verse = {}
 times = {}
 
-rus_verse = {'-ru', '-rus'}
-eng_verse = {'-en', '-eng'}
 	
 if __name__ == "__main__":
 	if len(sys.argv) > 1:	
-		if sys.argv[1] in rus_verse:
-			hello_uni('Доброй ночи!', 'Доброго утра!', 'Доброго дня!', 'Доброго вечера!')
-		elif sys.argv[1] in eng_verse:
-			hello_uni('Good night!', 'Good morning!', 'Good day!', 'Good evening!')
-		else:
-			print("Пока не завезли=( так что будет English")
-			hello_uni('Good night!', 'Good morning!', 'Good day!', 'Good evening!')
+		hello_uni('-ru', '-rus', 'Доброй ночи!', 'Доброго утра!', 'Доброго дня!', 'Доброго вечера!')
+		hello_uni('-en', '-eng', 'Good night!', 'Good morning!', 'Good day!', 'Good evening!')
+		if sys.argv[1] != verse:
+			times.update ({0:'Good night!', 1:'Good morning!', 2:'Good day!', 3:'Good evening!'})
+			print("такого языка пока не завезли, так что будет English")
+			print (times.get(hours))	
+		
 	else:
-		hello_uni('Good night!', 'Good morning!', 'Good day!', 'Good evening!')
+		times.update ({0:'Good night!', 1:'Good morning!', 2:'Good day!', 3:'Good evening!'})
+		print (times.get(hours))
 
-print (times.get(hours))
