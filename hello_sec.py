6838add061f40695f1d6bcc02731ae53af318d9e
Igor <fire.ex29@gmail.com>
2019-03-08 13:33:35 +0300

    вспомгательная функция при вводе exit возвращает None, в languages_hello добавил цикл while с условиями. в тело тьак же добавил цикл while

--- hello_sec.py
+++ hello_sec.py
@@ -18,34 +18,32 @@ def languages_hello(name_file,keys):
 	'''Принимает путь к файлу с словарём и сокращение языка.
 		Разбирает файл словаря.
 		Возвращает варианты ответов на выбраном языке.'''
+	while name_file and keys != None:
+		try:	
+			with open(name_file.rstrip(' '), 'r') as read_file:
+				for item in read_file.readlines():
+					data = item.strip('\n').split(',')
+					if keys in data[0]:
+						return data[1:]
+				keys = get_key_or_exit('Введите другой язык')
+				return languages_hello(name_file,keys)
 
-	try:
+		except FileNotFoundError:
+			name_file = get_key_or_exit('Введите правильный путь словаря')
+			return languages_hello(name_file, keys)
 
-		with open(name_file.rstrip(' '), 'r') as read_file:
-			for item in read_file.readlines():
-				data = item.strip('\n').split(',')
-				if keys in data[0]:
-					return data[1:]
-			keys = get_key_or_exit('Введите другой язык')
-			return languages_hello(name_file,keys)
-
-	except FileNotFoundError:
-		name_file = get_key_or_exit('Введите правильный путь словаря')
-		return languages_hello(name_file ,keys)
-	
-		
 def get_key_or_exit(message):
 
 	'''	просит ввести верное значение.
 		возвращает ввод или выходит'''
 
 	key = input(message+' или exit:\n')
-	if key.lower() == 'exit':
-		print('До свидания!')
-		exit()
-	elif not key:
+	if not key:
 		print("Введено пустое значение")
 		return get_key_or_exit(message)
+	elif key == 'exit':
+		print('Пока-пока')
+		return
 	else:
 		return key
 
@@ -54,4 +52,7 @@ data = languages_hello(args.dict,args.lang)
 key = get_time()
 
 if __name__ == '__main__':
-	print("{}!".format(data[key].capitalize()))
+	while data and key != None:
+		print("{}!".format(data[key].capitalize()))
+		break
+		
